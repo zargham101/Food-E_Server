eb666f8bf730a2e873eac2b26fc5be5200442cfd
AliyanJamshaid <aliyanjamshaid789@gmail.com>
2024-08-02 23:08:33 +0500

    update

--- index.js
+++ index.js
@@ -4,26 +4,22 @@ const port = 5000;
 const cors = require("cors");
 
 const dbConnection = require("./db");
-
+app.use(express.json());
 app.use(
   cors({
     origin:
       //"http://localhost:3000",
       "https://food-e-client.vercel.app", // Your frontend origin
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
 
 dbConnection();
 
-app.use(express.json());
-
 app.use("/api", require("./Routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
 app.use("/api", require("./Routes/OrderData"));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server is running on port ${process.env.PORT || port}`);
 });
